20a2781d4a8fc6780d5d5d67a5d2fd4065c841ff
Mostafa <mostafaghazal210@gmail.com>
2024-10-07 19:24:08 +0300

    moved images to public folder

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -4,12 +4,12 @@ import CurrencyConverter from "./components/CurrencyConverter";
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center" style={{
-      backgroundImage: 'url("/src/images/background.jpg")',
+      backgroundImage: 'url("/public/background.jpg")',
       backgroundSize: 'cover',
       backgroundPosition: 'center'
     }}>
     <div className="absolute top-6 left-10">
-      <img src="/src/images/logo.png" alt="Logo" className="h-20" />
+      <img src="/public/logo.png" alt="Logo" className="h-20" />
     </div>
 
     <div className="container">
