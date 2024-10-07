cfbde2c64d6f20fe597e2807e60e3a6a0ca167ef
Mostafa <mostafaghazal210@gmail.com>
2024-10-07 19:59:01 +0300

    updates

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -4,12 +4,12 @@ import CurrencyConverter from "./components/CurrencyConverter";
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center" style={{
-      backgroundImage: 'url("/public/background.jpg")',
+      backgroundImage: 'url("./background.jpg")',
       backgroundSize: 'cover',
       backgroundPosition: 'center'
     }}>
     <div className="absolute top-6 left-10">
-      <img src="/public/logo.png" alt="Logo" className="h-20" />
+      <img src="./logo.png" alt="Logo" className="h-20" />
     </div>
 
     <div className="container">
